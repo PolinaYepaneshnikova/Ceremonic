0ee83414a247b63c436851bd3da37d661f7cab87
SergeyCherevan <CherevanSergeyLugansk@gmail.com>
2023-01-13 11:53:36 +0200

    clear solution add: ASP.NET Core in Docker, React.JS in Docker & gninx as proxy

---
+++ CeremonicBackend/Controllers/UsersController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
+
+namespace CeremonicBackend.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetUsers()
+        {
+            var users = new[]
+            {
+                new { Name = "Alice" },
+                new { Name = "Paul" },
+                new { Name = "Anderew" },
+            };
+
+            return Ok(users);
+        }
+    }
+}
