fb6358d982372db36657f4b3fca50c92317040cc
SergeyCherevan <CherevanSergeyLugansk@gmail.com>
2023-03-13 17:02:45 +0200

    add: FilesController & FileRepository

---
+++ CeremonicBackend/Repositories/Interfaces/IFileRepository.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Threading.Tasks;
+using System;
+
+namespace CeremonicBackend.Repositories.Interfaces
+{
+    public interface IFileRepository
+    {
+        Task<PhysicalFileResult> GetByName(string folderName, string fileName);
+
+        Task<string> Add(string folderName, IFormFile file);
+
+        Task Delete(string folderName, string fileName);
+    }
+}
