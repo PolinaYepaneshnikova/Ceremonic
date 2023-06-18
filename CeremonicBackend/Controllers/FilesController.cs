7011a54df0f6963519998cd32abb6e3277005236
SergeyCherevan <CherevanSergeyLugansk@gmail.com>
2023-06-18 23:36:40 +0300

    fix: get avatar by user id

--- CeremonicBackend/Controllers/FilesController.cs
+++ CeremonicBackend/Controllers/FilesController.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 
 using CeremonicBackend.Repositories;
 using CeremonicBackend.Repositories.Interfaces;
+using CeremonicBackend.Services.Interfaces;
+using CeremonicBackend.WebApiModels;
 
 namespace CeremonicBackend.Controllers
 {
@@ -15,9 +17,20 @@ namespace CeremonicBackend.Controllers
     {
 
         IFileRepository _fileRepository { get; set; }
-        public FilesController(IWebHostEnvironment env)
+        IUserService _userService { get; set; }
+        IWeddingService _weddingService { get; set; }
+        IProviderService _providerService { get; set; }
+        public FilesController(
+            IWebHostEnvironment env, 
+            IUserService userService, 
+            IWeddingService weddingService, 
+            IProviderService providerService
+        )
         {
             _fileRepository = new FileRepository(this, env);
+            _userService = userService;
+            _weddingService = weddingService;
+            _providerService = providerService;
         }
 
         [HttpGet]
@@ -31,8 +44,23 @@ namespace CeremonicBackend.Controllers
             => await _fileRepository.GetByName("Images", fileName);
 
         [HttpGet]
-        [Route("avatar/{fileName}")]
-        public async Task<ActionResult> Avatar(string fileName)
-            => await _fileRepository.GetByName("Avatars", fileName);
+        [Route("avatar/{userId}")]
+        public async Task<ActionResult> Avatar(int userId)
+        {
+            string email = await _userService.GetEmailById(userId);
+
+            if (await _userService.GetRoleByEmail(email) == "User")
+            {
+                WeddingApiModel wedding = await _weddingService.Get(userId);
+
+                return await _fileRepository.GetByName("Avatars", wedding.Avatar1FileName);
+            }
+            else
+            {
+                ProviderApiModel provider = await _providerService.Get(userId);
+
+                return await _fileRepository.GetByName("Avatars", provider.AvatarFileName);
+            }
+        }
     }
 }
